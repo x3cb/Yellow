cd70f854f09dc7397c9584124509858a6a3644ca
x3cb <allmyscamas@proton.me>
2024-08-05 13:46:58 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="The Darknet Directory made by Community.">
+    <meta name="keywords" content="Yellow, darknet, deepweb, darkweb, dw, hidden wiki, wiki, link, links, list, dark, web, net, deep, onion, onions, photo, photos, video, videos, quality, counterfeit, bill, bills, card, carding, drug, drugs, market, markets, marketplace, marketplaces, cute, good, annuaire, dread, forum, forums, hack, hacking, anonymous, anon, buy, free, easy, money, yellow">
+    <title>Yellow</title>
+    <link rel="icon" href="styles/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="container">
+            <header>
+                <nav>
+                    <div class="logo-container">
+                        <a href="index.php"><img src="styles/logo.png" alt="Logo" class="logo"></a>
+                    </div>
+                    <ul class="nav-links">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About us</a></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <footer>
+                <p>&copy; <?php echo date("Y"); ?> Yellow. All rights Reserved.</p>
+            </footer>
+        </div>
+    </div>
+</body>
+
+</html>
